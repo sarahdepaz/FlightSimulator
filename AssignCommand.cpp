a36de860df967f155fb52b81ffcd942085abce62
ornush <tapeta01@walla.com>
2020-01-02 16:56:49 +0200

    new

--- AssignCommand.cpp
+++ AssignCommand.cpp
@@ -3,32 +3,32 @@
 #include "Evaluator.h"
 #include <stdio.h>
 // Assignement of command
-AssignCommand::AssignCommand(map<string, double>* symbolTable,
-                             map<string, string>* pathToVar,
-                             map<string, vector<string>>* bindedVarTable,
-                             Modifier* modifier) {
-  _symbolTable = symbolTable;
-  _pathToVar = pathToVar;
-  _bindedVarTable = bindedVarTable;
-  _modifier = modifier;
-  _argumentsAmount = 2;
+AssignCommand::AssignCommand(map<string, double> *symbolTable,
+                             map<string, string> *pathToVar,
+                             map<string, vector<string>> *bindedVarTable,
+                             Modifier *modifier) {
+    _symbolTable = symbolTable;
+    _pathToVar = pathToVar;
+    _bindedVarTable = bindedVarTable;
+    _modifier = modifier;
+    _argumentsAmount = 2;
 }
 
 /*
 * execute ->
-* assigns variable it's requested value. 
+* assigns variable it's requested value.
 */
-int AssignCommand::execute(vector<string>& arguments, unsigned int index) {
-  if ((arguments.size() - 1) < _argumentsAmount)
-    throw "Arguments amount is lower than " + to_string(_argumentsAmount);
-  // if variable doesn't exist, sends an exception.
-  if (_symbolTable->find(arguments[index - 1]) == _symbolTable->end())
-    throw "The variable " + arguments[index - 1] + " is not defined";
-  string argument = StringHelper::getArgument(arguments);
-  int varIndex = index - 1;
-  double value = Evaluator::evaluate(arguments, &(++index), _symbolTable);
-  // calls the modifier who is responsible to managing the variables.
-  _modifier->setVariableValue(arguments[varIndex], value);
-  _modifier->updateSimulator(arguments[varIndex]);
-  return index;
+int AssignCommand::execute(vector<string> &arguments, unsigned int index) {
+    if ((arguments.size() - 1) < _argumentsAmount)
+        throw "Arguments amount is lower than " + to_string(_argumentsAmount);
+    // if variable doesn't exist, sends an exception.
+    if (_symbolTable->find(arguments[index - 1]) == _symbolTable->end())
+        throw "The variable " + arguments[index - 1] + " is not defined";
+    string argument = StringHelper::getArgument(arguments);
+    int varIndex = index - 1;
+    double value = Evaluator::evaluate(arguments, &(++index), _symbolTable);
+    // calls the modifier who is responsible to managing the variables.
+    _modifier->setVariableValue(arguments[varIndex], value);
+    _modifier->updateSimulator(arguments[varIndex]);
+    return index;
 }
--- BinaryExpression.h
+++ BinaryExpression.h
@@ -4,21 +4,24 @@
 #include "Expression.h"
 
 using namespace std;
-
-/*
- * Abstract class that represents expressions
+/**
+ *
+ * BinaryExpression class
+ *
+ * Abstract base class that represents mathematical expressions
  * who operate over 2 expressions.
+ *
  * */
 class BinaryExpression : public Expression {
- protected:
-  Expression& _left_expression;
-  Expression& _right_expression;
- public:
-  BinaryExpression(Expression& left_expression,
-                   Expression& right_expression):
-      _left_expression(left_expression),
-      _right_expression(right_expression) {}
-  virtual double calculate(vector<string>& arguments, int index) = 0;
+protected:
+    Expression &_left_expression;
+    Expression &_right_expression;
+public:
+    BinaryExpression(Expression &left_expression,
+                     Expression &right_expression) :
+            _left_expression(left_expression),
+            _right_expression(right_expression) {}
+    virtual double calculate(vector<string> &arguments, int index) = 0;
 };
 
-#endif	//	!BINARY_EXPRESSION_H
+#endif    //	!BINARY_EXPRESSION_H
